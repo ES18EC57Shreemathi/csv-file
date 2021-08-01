18a7a4e8c24a7c8020061e89cadc629ffac7b5c1
ES18EC57Shreemathi <88272616+ES18EC57Shreemathi@users.noreply.github.com>
2021-08-01 16:41:04 +0530

    Create DataImport.java

---
+++ http:/DataImport.java
@@ -0,0 +1,16 @@
+package programs;
+
+public class DataImport {
+
+            public static void main(String[] args) {
+
+                        CSVLoader csvLoader=new CSVLoader();
+
+            String fileName="D:/File1.csv";
+
+            csvLoader.loadCSV(fileName);
+
+                    }
+
+}
+
